d0aa623125b2d59cc5ce717f44395bfaaf5adff7
dingzhiwen <dingzw@ibaodashi.com>
2019-11-22 10:26:13 +0800

    con

---
+++ SMRDydeployment/Lib/DyLabel.h
@@ -0,0 +1,17 @@
+//
+//  DyLabel.h
+//  SMRDydeploymentDemo
+//
+//  Created by Tinswin on 2019/11/21.
+//  Copyright © 2019 Tinswin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DyLabel : UILabel
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ SMRDydeployment/Lib/DyLabel.m
@@ -0,0 +1,18 @@
+//
+//  DyLabel.m
+//  SMRDydeploymentDemo
+//
+//  Created by Tinswin on 2019/11/21.
+//  Copyright © 2019 Tinswin. All rights reserved.
+//
+
+#import "DyLabel.h"
+#import "SMRDydeployment.h"
+
+@implementation DyLabel
+
+- (void)dyViewDidLoad:(SMRDyView *)dyView {
+    [super dyViewDidLoad:dyView];
+}
+
+@end
